43bc32394e3f15c8c59d1c3ab37244bfd6218c35
Alegraleeson <wingmonkee@yahoo.com>
2022-06-22 15:10:12 -0500

    problem 8

--- lab.js
+++ lab.js
@@ -120,6 +120,11 @@ let passingScore = 7;
 
 //CODE HERE
 
+while (score < passingScore) {
+  console.log("Your score is not high enough")
+  score ++
+}
+
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
